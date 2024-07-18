c38d3caad1c252a7252747b55e23a4d99d8d5bcc
CHIKA0411 <121760538+CHIKA0411@users.noreply.github.com>
2024-07-18 18:40:35 +0530

    Add files via upload

---
+++ OUTPUT/O_1.java
@@ -0,0 +1,16 @@
+import java.time.Year;
+
+public class O_1 {
+    public static void main(String[] args) {
+        int x=10,y=20;
+        char z='G';
+        String str="abha";
+        System.out.println(x);
+        System.out.println(x+ y);
+        System.out.println(z);
+        System.out.println(x+" "+y);
+        System.out.print(str+" ");
+        System.out.println("Mahato");
+
+    }
+}
