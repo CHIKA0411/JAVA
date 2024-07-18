547cb27f07f242604cb0161ee9c9225a745257bb
CHIKA0411 <121760538+CHIKA0411@users.noreply.github.com>
2024-07-18 18:41:18 +0530

    Add files via upload

---
+++ OUTPUT/O_2.java
@@ -0,0 +1,13 @@
+public class O_2 {
+    public static void main(String[] args) {
+        int x=100,y=200;
+        System.out.format("Value of x is %d and y is : %d",x,y);
+        double z = (float) Math.PI;
+        System.out.println(z);
+        System.out.format("Value of PI is %2f",z);
+        System.out.format("Value of PI is %5.2f",z);
+        System.out.format("Value of PI is %05.2f",z);
+        System.out.printf("Value of x is %d and y is : %d",x,y);
+    }
+
+}
